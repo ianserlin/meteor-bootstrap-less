fdc495c3484f803724f3cb2df0dc00a50a448194
Ian Serlin <ian@ianserlin.com>
2013-11-10 13:42:02 -0800

    updating to support meteor 0.6.5+ and Bootstrap 3.0.2

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 [Bootstrap](http://twitter.github.com/bootstrap) The sleek, intuitive, and powerful front-end framework for faster and easier web development.
 
+Currently at Bootstrap version _3.0.2_
+
 ## How is this different from the official Meteor Bootstrap package?
 
 The official package contains the compiled CSS files, meaning you don't get all the LESSy goodness like mixins, variables, etc that makes customizing Bootstrap super awesome. This unofficial package contains the source less files, but modified so that they can be loaded properly by Meteor's `less` package.
@@ -12,7 +14,7 @@ The official package contains the compiled CSS files, meaning you don't get all 
 1. `npm install -g meteorite` (if not already installed)
 2. `mrt add less`
 3. `mrt add bootstrap-less`
-4. From the root folder of your project, add this import line to your project's less file: `@import ".meteor/meteorite/packages/bootstrap-less/lib/less/bootstrap.lessimport";`
+4. From the root folder of your project, add this import line to your project's less file: `@import "packages/bootstrap-less/lib/less/bootstrap.lessimport";`
 
 ## Meteor-Bootstrap-Less License
 
--- package.js
+++ package.js
@@ -1,4 +1,4 @@
-var path = require('path');
+var path = Npm.require('path');
 
 Package.describe({
   summary: "Twitter's Bootstrap Packaged for Meteor, using Less"
@@ -6,58 +6,60 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.use('jquery');
-  api.add_files(path.join('lib', 'less', 'accordian.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'alerts.lessimport'), 'server');
+  api.add_files(path.join('lib', 'less', 'badges.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'breadcrumbs.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'button-groups.lessimport'), 'server');
+  api.add_files(path.join('lib', 'less', 'buttons.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'carousel.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'close.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'code.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'component-animations.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'dropdowns.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'forms.lessimport'), 'server');
+  api.add_files(path.join('lib', 'less', 'glyphicons.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'grid.lessimport'), 'server');
-  api.add_files(path.join('lib', 'less', 'hero-unit.lessimport'), 'server');
-  api.add_files(path.join('lib', 'less', 'labels-badges.lessimport'), 'server');
-  api.add_files(path.join('lib', 'less', 'layouts.lessimport'), 'server');
+  api.add_files(path.join('lib', 'less', 'input-groups.lessimport'), 'server');
+  api.add_files(path.join('lib', 'less', 'jumbotron.lessimport'), 'server');
+  api.add_files(path.join('lib', 'less', 'labels.lessimport'), 'server');
+  api.add_files(path.join('lib', 'less', 'list-group.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'media.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'mixins.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'modals.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'navbar.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'navs.lessimport'), 'server');
+  api.add_files(path.join('lib', 'less', 'normalize.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'pager.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'pagination.lessimport'), 'server');
+  api.add_files(path.join('lib', 'less', 'panels.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'popovers.lessimport'), 'server');
+  api.add_files(path.join('lib', 'less', 'print.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'progress-bars.lessimport'), 'server');
-  api.add_files(path.join('lib', 'less', 'reset.lessimport'), 'server');
-  api.add_files(path.join('lib', 'less', 'responsive-1200px-min.lessimport'), 'server');
-  api.add_files(path.join('lib', 'less', 'responsive-767px-max.lessimport'), 'server');
-  api.add_files(path.join('lib', 'less', 'responsive-768px-979px.lessimport'), 'server');
-  api.add_files(path.join('lib', 'less', 'responsive-navbar.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'responsive-utilities.lessimport'), 'server');
-  api.add_files(path.join('lib', 'less', 'responsive.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'scaffolding.lessimport'), 'server');
-  api.add_files(path.join('lib', 'less', 'sprites.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'tables.lessimport'), 'server');
+  api.add_files(path.join('lib', 'less', 'theme.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'thumbnails.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'tooltip.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'type.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'utilities.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'variables.lessimport'), 'server');
   api.add_files(path.join('lib', 'less', 'wells.lessimport'), 'server');
-  api.add_files(path.join('lib', 'js', 'bootstrap-affix.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-alert.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-button.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-carousel.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-collapse.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-dropdown.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-modal.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-popover.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-scrollspy.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-tab.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-tooltip.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-transition.js'), 'client');
-  api.add_files(path.join('lib', 'js', 'bootstrap-typeahead.js'), 'client');
-  api.add_files(path.join('lib', 'img', 'glyphicons-halflings.png'), 'client');
-  api.add_files(path.join('lib', 'img', 'glyphicons-halflings-white.png'), 'client');
+
+  api.add_files(path.join('lib', 'js', 'affix.js'), 'client');
+  api.add_files(path.join('lib', 'js', 'alert.js'), 'client');
+  api.add_files(path.join('lib', 'js', 'button.js'), 'client');
+  api.add_files(path.join('lib', 'js', 'carousel.js'), 'client');
+  api.add_files(path.join('lib', 'js', 'collapse.js'), 'client');
+  api.add_files(path.join('lib', 'js', 'dropdown.js'), 'client');
+  api.add_files(path.join('lib', 'js', 'modal.js'), 'client');
+  api.add_files(path.join('lib', 'js', 'scrollspy.js'), 'client');
+  api.add_files(path.join('lib', 'js', 'tab.js'), 'client');
+  api.add_files(path.join('lib', 'js', 'tooltip-and-popover.js'), 'client');
+  api.add_files(path.join('lib', 'js', 'transition.js'), 'client');
+
+  api.add_files(path.join('lib', 'fonts', 'glyphicons-halflings-regular.eot'), 'client');
+  api.add_files(path.join('lib', 'fonts', 'glyphicons-halflings-regular.svg'), 'client');
+  api.add_files(path.join('lib', 'fonts', 'glyphicons-halflings-regular.ttf'), 'client');
+  api.add_files(path.join('lib', 'fonts', 'glyphicons-halflings-regular.woff'), 'client');
 });
